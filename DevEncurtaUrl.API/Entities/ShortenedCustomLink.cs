cad6778b0adad8d05ab125e2a5bb1dcb082f47fb
Samuel B. Oldra <samuel.oldra@gmail.com>
2023-02-17 00:00:00 -0300

    Refactoring controllers, entities, models e context

--- DevEncurtaUrl.API/Entities/ShortenedCustomLink.cs
+++ DevEncurtaUrl.API/Entities/ShortenedCustomLink.cs
@@ -2,26 +2,34 @@ namespace DevEncurtaUrl.API.Entities
 {
     public class ShortenedCustomLink
     {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string DestinationLink { get; set; }
+
+        public string ShortenedLink { get; set; }
+
+        public string Code { get; set; }
+
+        public string CreatedAt { get; set; }
+
         private ShortenedCustomLink() { }
+
         public ShortenedCustomLink(string title, string destinationLink, string domain)
         {
             var code = title.Split(" ")[0];
 
-            Title = title; // ultimo-artigo Blog
+            Title = title;
             DestinationLink = destinationLink;
             ShortenedLink = $"{domain}/{code}";
+
             Code = code;
             CreatedAt = DateTime.Now.ToShortDateString();
         }
 
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string ShortenedLink { get; set; }
-        public string DestinationLink { get; set; }
-        public string Code { get; set; }
-        public string CreatedAt { get; set; }
-
-        public void Update(string title, string destinationLink) {
+        public void Update(string title, string destinationLink)
+        {
             Title = title;
             DestinationLink = destinationLink;
         }
--- DevEncurtaUrl.API/Models/AddOrUpdateShortenedLinkModel.cs
+++ DevEncurtaUrl.API/Models/AddOrUpdateShortenedLinkModel.cs
@@ -3,7 +3,9 @@ namespace DevEncurtaUrl.API.Models
     public class AddOrUpdateShortenedLinkModel
     {
         public int Id { get; set; }
+
         public string Title { get; set; }
+
         public string DestinationLink { get; set; }
     }
 }
