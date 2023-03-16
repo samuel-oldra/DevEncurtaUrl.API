de0473710058a936d221f329850ee626c372b506
Samuel B. Oldra <samuel.oldra@gmail.com>
2023-03-16 19:00:00 -0300

    Adicionados comentários no SWAGGER #1

--- DevEncurtaUrl.API/Controllers/ShortenedLinksController.cs
+++ DevEncurtaUrl.API/Controllers/ShortenedLinksController.cs
@@ -15,8 +15,15 @@ namespace DevEncurtaUrl.API.Controllers
         public ShortenedLinksController(DevEncurtaUrlDbContext context)
             => _context = context;
 
+        // GET: api/shortenedLinks
+        /// <summary>
+        /// Listagem de Links Encurtados
+        /// </summary>
+        /// <returns>Lista de Links Encurtados</returns>
+        /// <response code="200">Sucesso</response>
         [HttpGet]
-        public IActionResult Get()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult GetAll()
         {
             Log.Information("Endpoint - GET: api/shortenedLinks");
 
@@ -25,30 +32,51 @@ namespace DevEncurtaUrl.API.Controllers
             return Ok(links);
         }
 
+        // GET: api/shortenedLinks/{id}
+        /// <summary>
+        /// Detalhes do Link Encurtado
+        /// </summary>
+        /// <param name="id">ID do Link Encurtado</param>
+        /// <returns>Mostra um Link Encurtado</returns>
+        /// <response code="200">Sucesso</response>
+        /// <response code="404">Não encontrado</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult GetById(int id)
         {
+            Log.Information("Endpoint - GET: api/shortenedLinks/{id}");
+
             var link = _context.Links.SingleOrDefault(l => l.Id == id);
 
             if (link == null)
-                return NotFound();
+                return NotFound("Link Encurtado não encontrado.");
 
             return Ok(link);
         }
 
+        // POST: api/shortenedLinks
         /// <summary>
-        /// Cadastrar um link encurtado
+        /// Cadastro de Link Encurtado
         /// </summary>
         /// <remarks>
-        /// { "title": "ultimo-artigo Blog", "destinationLink" : "https://www.luisdev.com.br/2023/02/05/atributos-no-c-como-criar-e-aplicar-em-seu-projeto" }
+        /// Requisição:
+        /// {
+        ///     "title": "GitHub Samuel B. Oldra",
+        ///     "destinationLink" : "https://github.com/samuel-oldra"
+        /// }
         /// </remarks>
-        /// <param name="model">Dados de link</param>
-        /// <returns>Objeto recém-criado</returns>
-        /// <response code="201">Sucesso!</response>
+        /// <param name="model">Dados do Link Encurtado</param>
+        /// <returns>Objeto criado</returns>
+        /// <response code="201">Sucesso</response>
+        /// <response code="400">Dados inválidos</response>
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult Post(AddOrUpdateShortenedLinkModel model)
         {
+            Log.Information("Endpoint - POST: api/shortenedLinks");
+
             var domain = HttpContext.Request.Host.Value;
 
             var link = new ShortenedCustomLink(model.Title, model.DestinationLink, domain);
@@ -56,16 +84,37 @@ namespace DevEncurtaUrl.API.Controllers
             _context.Links.Add(link);
             _context.SaveChanges();
 
-            return CreatedAtAction("GetById", new { id = link.Id }, link);
+            return CreatedAtAction(nameof(GetById), new { id = link.Id }, link);
         }
 
+        // PUT: api/shortenedLinks/{id}
+        /// <summary>
+        /// Atualiza um Link Encurtado
+        /// </summary>
+        /// <remarks>
+        /// Requisição:
+        /// {
+        ///     "title": "LinkedIn Samuel B. Oldra",
+        ///     "destinationLink" : "https://www.linkedin.com/in/samuel-oldra/"
+        /// }
+        /// </remarks>
+        /// <param name="id">ID do Link Encurtado</param>
+        /// <param name="model">Dados do Link Encurtado</param>
+        /// <response code="204">Sucesso</response>
+        /// <response code="400">Dados inválidos</response>
+        /// <response code="404">Não encontrado</response>
         [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Put(int id, AddOrUpdateShortenedLinkModel model)
         {
+            Log.Information("Endpoint - PUT: api/shortenedLinks/{id}");
+
             var link = _context.Links.SingleOrDefault(l => l.Id == id);
 
             if (link == null)
-                return NotFound();
+                return NotFound("Link Encurtado não encontrado.");
 
             link.Update(model.Title, model.DestinationLink);
 
@@ -75,13 +124,24 @@ namespace DevEncurtaUrl.API.Controllers
             return NoContent();
         }
 
+        // DELETE: api/shortenedLinks/{id}
+        /// <summary>
+        /// Deleta um Link Encurtado
+        /// </summary>
+        /// <param name="id">ID do Link Encurtado</param>
+        /// <response code="204">Sucesso</response>
+        /// <response code="404">Não encontrado</response>
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Delete(int id)
         {
+            Log.Information("Endpoint - DELETE: api/shortenedLinks/{id}");
+
             var link = _context.Links.SingleOrDefault(l => l.Id == id);
 
             if (link == null)
-                return NotFound();
+                return NotFound("Link Encurtado não encontrado.");
 
             _context.Links.Remove(link);
             _context.SaveChanges();
@@ -89,13 +149,23 @@ namespace DevEncurtaUrl.API.Controllers
             return NoContent();
         }
 
+        // GET: api/shortenedLinks/{code}
+        /// <summary>
+        /// Redireciona o Link Encurtado
+        /// </summary>
+        /// <param name="code">Código do Link Encurtado</param>
+        /// <returns>Redireciona um Link Encurtado</returns>
+        /// <response code="404">Não encontrado</response>
         [HttpGet("/{code}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult RedirectLink(string code)
         {
+            Log.Information("Endpoint - GET: api/shortenedLinks/{code}");
+
             var link = _context.Links.SingleOrDefault(l => l.Code == code);
 
             if (link == null)
-                return NotFound();
+                return NotFound("Link Encurtado não encontrado.");
 
             return Redirect(link.DestinationLink);
         }
